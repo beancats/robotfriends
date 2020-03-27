87c04b55ed0d75ef5ab5d5023a1b673d9b5786d7
Ryan <beancatsock@gmail.com>
2020-03-27 15:39:35 +1300

    Updates

---
+++ precache-manifest.9a5d1a950d0bca96babd741ccef24802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d491481a0d57ce9d00a01f1751c74b",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "9eabbc230ab34f21e9fc",
+    "url": "/robotfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "74ebcb8c66522223d363",
+    "url": "/robotfriends/static/css/main.b68b1b20.chunk.css"
+  },
+  {
+    "revision": "9eabbc230ab34f21e9fc",
+    "url": "/robotfriends/static/js/2.d09e8277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.d09e8277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ebcb8c66522223d363",
+    "url": "/robotfriends/static/js/main.9f115bf3.chunk.js"
+  },
+  {
+    "revision": "e0dbe2145c01752952d4",
+    "url": "/robotfriends/static/js/runtime-main.e6fc5b87.js"
+  }
+]);
